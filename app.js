2e68351dc57db789ac65ef981e53efe46dc82081
Gian <Gianluca.bonatto1@gmail.com>
2024-03-12 21:00:31 -0300

    c8 - fiz cagada alterando o site e tive que voltar tudo, vou fazer de um jeito e depois vou melhora-lo

---
+++ app.js
@@ -0,0 +1,14 @@
+function changeImage (imageName) {
+    var displayImage = document.getElementById ("displayImage");
+    displayImage.src = imageName;
+}
+
+function changeVideo (videoName) {
+    var displayVideo = document.getElementById ("displayVideo");
+    displayVideo.src = videoName;
+}
+
+// function changeVideo (videoName) {
+//     var displayVideo = document.getElementById ("displayVideo");
+//     displayVideo.src = videoName;
+// }
